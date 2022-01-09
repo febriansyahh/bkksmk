2ff2e38b3ae2ec1b96790fdc3f0dc64d29938a98
febriansyahh <80746617+febriansyahh@users.noreply.github.com>
2022-01-09 11:39:30 +0700

    up

--- landing/perTerdaftar.php
+++ landing/perTerdaftar.php
@@ -11,7 +11,17 @@
         <div class="col-lg-4 mb-6 mb-lg-4" >
           <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
           <h4 class="title">
+            <?php
+            if($data['logo'] != NULL){
+            ?>
             <img src=<?php echo "file_data/logo/".$data['logo'] ?> alt="" style="width:40px; height:40px"> -
+            <?php
+            }else{
+            ?>
+            <img src=<?php echo "file_data/logo/default.png" ?> alt="" style="width:40px; height:40px"> -
+            <?php
+            }
+            ?>
             <?php echo $data['nmPerusahaan'] ?></h4>
             <p class="description" style="text-align: justify;"> Perusahaan 
               <?php echo $data['nmPerusahaan'] ." dengan alamat email ". $data['email']
